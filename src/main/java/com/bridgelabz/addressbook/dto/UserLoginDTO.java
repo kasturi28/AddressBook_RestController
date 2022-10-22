294334097b4162930631ed8cf036cfa80b4daa7f
kasturi28 <kasturipansare2818@gmail.com>
2022-10-22 23:17:50 +0530

    Added:Addressbook controller with user registration and login with validation of user name, email and password

--- src/main/java/com/bridgelabz/addressbook/dto/UserLoginDTO.java
+++ src/main/java/com/bridgelabz/addressbook/dto/UserLoginDTO.java
@@ -2,9 +2,17 @@ package com.bridgelabz.addressbook.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+
 public @Data class UserLoginDTO {
 
+    @NotEmpty(message = "User name cannot be empty")
+    @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$", message = "User name Invalid")
     private String userName;
+
+    @NotEmpty(message = "password cannot be empty")
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$", message = "password Invalid")
     private String password;
 
 
--- src/main/java/com/bridgelabz/addressbook/dto/UserRegistrationDTO.java
+++ src/main/java/com/bridgelabz/addressbook/dto/UserRegistrationDTO.java
@@ -2,10 +2,21 @@ package com.bridgelabz.addressbook.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+
 public @Data class UserRegistrationDTO {
 
+    @NotEmpty(message = "User name cannot be empty")
+    @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$", message = "User name Invalid")
     private String userName;
+
+    @Email(message = "Enter correct email address")
     private String userEmail;
+
+    @NotEmpty(message = "password cannot be empty")
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$", message = "password Invalid")
     private String password;
 
 
